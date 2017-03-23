7bef3e745f55fa60e44602aa90ca57029e10ec7c
Sankar Gireesan Nair <sankargireesan@ccs.neu.edu>
2017-03-22 23:25:17 -0400

    with connection string

--- assignment/model/models.server.js
+++ assignment/model/models.server.js
@@ -1,6 +1,15 @@
 module.exports = function () {
     var mongoose = require('mongoose');
-    mongoose.connect('mongodb://localhost/wbdv-asssignment');
+
+    var connectionString = 'mongodb://localhost/wbdv-asssignment';
+    if(process.env.MLAB_USERNAME) {
+        connectionString = process.env.MLAB_USERNAME + ":" +
+            process.env.MLAB_PASSWORD + "@" +
+            process.env.MLAB_HOST + ':' +
+            process.env.MLAB_PORT + '/' +
+            process.env.MLAB_APP_NAME;
+    }
+    mongoose.connect(connectionString);
 
     var userModel = require("./user/user.model.server")();
     var websiteModel = require("./website/website.model.server")();
